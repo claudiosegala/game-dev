84bf0724aa47560d78f2990bb0dd5cb0a76a4919
claudiosegala <claudiosegalafilho@gmail.com>
2019-03-27 12:36:44 -0300

    removing old version

--- src/main.cpp
+++ src/main.cpp
@@ -7,42 +7,6 @@
 #include <Logger.h>
 #include <Util.h>
 
-void test () {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
-    SDL_Window* window = SDL_CreateWindow("Penguin", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 600, 0);
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF);
-    SDL_Texture* texture = IMG_LoadTexture(renderer, "/assets/img/ocean.jpg");
-    int textureWidth, textureHeight;
-    SDL_QueryTexture(texture, nullptr, nullptr, &textureWidth, &textureHeight);
-
-    SDL_Rect sourceRect = {0, 0, textureWidth, textureHeight};
-    SDL_Rect onScreenRect = {0, 0, 1024, 600};
-    SDL_RenderCopyEx(renderer, texture, &sourceRect, &onScreenRect, 0, nullptr, SDL_FLIP_NONE);
-    SDL_RenderPresent(renderer);
-
-    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
-    Mix_Music* music = Mix_LoadMUS("/assets/audio/boom.wav");
-    Mix_PlayMusic(music, 1);
-
-     while(true)
-     {
-         if(SDL_QuitRequested())
-         {
-             break;
-         }
-     }
-
-    Mix_FreeMusic(music);
-    Mix_CloseAudio();
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
-}
-
 int main(int argc, char** argv)
 {
     UNUSED(argc);
