ed31bb57f66102d6ba235540b08e41692bdbf915
claudiosegala <claudiosegalafilho@gmail.com>
2019-03-30 11:17:59 -0300

    Creating Face class

---
+++ include/Face.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Component.h"
+#include "GameObject.h"
+#include <string>
+
+namespace penguin {
+
+    class Face : public Component {
+        public:
+
+        Face(GameObject&);
+
+        void Damage(int);
+
+        void Update(float);
+
+        void Render();
+
+        bool Is(std::string);        
+
+        private:
+
+        int hitpoints;
+    };
+
+}
---
+++ src/Face.cpp
@@ -0,0 +1,34 @@
+#include "Face.h"
+#include "Component.h"
+#include "GameObject.h"
+
+namespace penguin {
+
+    Face::Face(GameObject& obj) : Component(obj) {
+        this->hitpoints = 30; // 30HP
+    }
+
+    void Face::Damage(int damage) {
+        this->hitpoints -= damage;
+
+        if (this->hitpoints <= 0) {
+            auto associatedSound = this->associated.GetComponent("Sound");
+
+            this->associated.RequestDelete();
+
+            if (associatedSound != nullptr) {
+                // TODO: how do I give play???
+                associatedSound->Play();
+            }
+        }
+    }
+
+    void Face::Update(float) {}
+
+    void Face::Render() {}
+
+    bool Face::Is(std::string type) {
+        return (type == "Face");
+    } 
+
+}
