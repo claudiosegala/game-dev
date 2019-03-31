9cd04661bc7ff73618df378c9a8547f4eb3b93f8
claudiosegala <claudiosegalafilho@gmail.com>
2019-03-31 20:08:34 -0300

    Adding functions that may be useful in the future

--- include/Util.h
+++ include/Util.h
@@ -1,1 +1,20 @@
 #define UNUSED(x) (void)x
+
+#include <algorithm>
+
+// namespace range
+// {
+
+//     template<typename Range, typename Function>
+//     Function for_each(Range& range, Function f) {
+//         return std::for_each(begin(range), end(range), f);
+//     }
+
+//     template<typename Range, typename Function>
+//     Function clear(Range& range, Function f) {
+//         auto it = std::remove_if(begin(range), end(range), f);
+
+//         return range.erase(it, end(range));
+//     }
+
+// }
