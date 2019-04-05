394964f81f61d67bea434b81a38b82d82f960099
claudiosegala <claudiosegalafilho@gmail.com>
2019-04-05 15:20:41 -0300

    Adding TileSet

---
+++ include/TileSet.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <GameObject.h>
+#include <Sprite.h>
+#include <string>
+
+namespace penguin {
+
+    class TileSet {
+        public:
+        
+        TileSet(GameObject&, int, int, std::string);
+        
+        void RenderTile(unsigned int, float, float);
+
+        int GetQuantityTiles() const;
+    
+        int GetTileWidth() const;
+
+        int GetTileHeight() const;
+
+        private:
+
+        Sprite tileSet;
+
+        int rows;
+
+        int columns;
+
+        int tileWidth;
+
+        int tileHeight;
+
+    };
+
+}
---
+++ src/TileSet.cpp
@@ -0,0 +1,35 @@
+#include "TileSet.h"
+
+namespace penguin {
+
+    TileSet::TileSet(GameObject& obj, int tileWidth, int tileHeight, std::string filename) : tileSet(obj, filename) {
+        this->tileWidth = tileWidth;
+        this->tileHeight = tileHeight;
+    }
+        
+    void TileSet::RenderTile(unsigned int index, float x, float y) {
+        auto idx = (int) index;
+        auto n = GetQuantityTiles();
+
+        if (idx < n) {
+            auto i = (idx % this->columns);
+            auto j = (idx / this->rows);
+
+            this->tileSet.SetClip(i * this->tileWidth, j * this->tileHeight, this->tileWidth, this->tileHeight);
+            this->tileSet.Render(x, y);
+        } 
+    }
+
+    int TileSet::GetQuantityTiles() const {
+        return this->tileWidth * this->tileHeight;
+    }
+
+    int TileSet::GetTileWidth() const {
+        return this->tileWidth;
+    }
+
+    int TileSet::GetTileHeight() const {
+        return this->tileHeight;
+    }
+
+}
