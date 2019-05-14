b9d6cc7f3081ea6fa3958439a28d61b74e864766
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-14 17:19:46 -0300

    Adding state

---
+++ src/State.cpp
@@ -0,0 +1,67 @@
+#include <State.h>
+
+State::State() : objectArray() {
+    this->started = false;
+    this->quitRequested = false;
+    this->popRequested = false;
+}
+
+State::~State() {
+    this->objectArray.clear();
+}
+
+std::weak_ptr<GameObject> State::AddObject (GameObject* gameObject) {
+    this->objectArray.emplace_back(gameObject);
+
+    if (this->started) {
+        gameObject->Start();
+    }
+
+    return this->objectArray.back();
+}
+
+std::weak_ptr<GameObject> State::GetObjectPtr(GameObject* gameObject) {
+    auto f = [&](const std::shared_ptr<GameObject>& _go) {
+        return _go.get() == gameObject;
+    };
+
+    auto ptr = std::find_if(this->objectArray.begin(), this->objectArray.end(), f);
+
+    return ptr != this->objectArray.end() 
+        ? std::weak_ptr<GameObject>(*ptr)
+        : std::weak_ptr<GameObject>();
+}
+
+bool State::PopRequested() {
+    return this->popRequested;
+}
+
+bool State::QuitRequested() {
+    return this->quitRequested;
+}
+
+void State::RenderArray() {
+    for (auto & object : this->objectArray) {
+        object->Render();
+    }
+}
+
+void State::StartArray() {
+    for (auto & object : this->objectArray) {
+        object->Start();
+    }
+}
+
+void State::UpdateArray(float dt) {
+    for (auto & object : this->objectArray) {
+        object->Update(dt);
+    }
+}
+
+void State::PruneArray() {
+    auto it = std::remove_if(this->objectArray.begin(), this->objectArray.end(), [&] (std::shared_ptr<GameObject>& o) { 
+        return o->IsDead();
+    });
+
+    this->objectArray.erase(it, this->objectArray.end());
+}
